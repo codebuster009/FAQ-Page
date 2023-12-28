6790eac96b15191faaca37cc6f32197ae4809805
kartavyaKloud <kartavya@kloudone.com>
2023-12-29 01:25:25 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Header/Index.jsx'
+import Questions from './Questions/Index.jsx'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Questions/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Questions/Index.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+const Questions = () => {
+    return (
+        <>
+            <div className="questions">
+            <h3>What is Frontend Mentor, and how will it help me</h3>
+            </div>
+            <div className="questions">
+            <h3>Is Frontend Mentor free</h3>
+            </div>
+            <div className="questions">
+            <h3>Can I use Frontend Mentor projects in muy portfolio</h3>
+            </div>
+            <div className="questions">
+            <h3>How can I get help if i'm stuck on a challenge</h3>
+            </div>
+        </>
+    )
+}
+
+export default Questions;
